c5e92ca865f8e92535f8a1f2550ccb38a6f7ad2b
Mottuqe Brid <mottuqebrid@gmail.com>
2025-03-29 15:11:48 +0600

    learning room

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { Server } = require("socket.io");
 const http = require("http");
-const { Socket } = require("dgram");
 const app = express();
 
 const expressServer = http.createServer(app);
@@ -9,12 +8,28 @@ const PORT = 3000;
 
 const io = new Server(expressServer);
 
-io.on("connection", function (socket) {
-  //   console.log("New user connected");
-  socket.on("chat", function (mes) {
-    // console.log(`message: ${mes}`);
-    io.emit("chat_transfer", mes);
-  });
+// io.on("connection", function (socket) {
+//   //   console.log("New user connected");
+//   socket.on("chat", function (mes) {
+//     // console.log(`message: ${mes}`);
+//     io.emit("chat_transfer", mes);
+//   });
+// });
+
+// create socket room
+io.on("connection", (socket) => {
+  // socket.join("dining-room");
+  socket.join("kitchen-room");
+  let room = io.sockets.adapter.rooms.get("kitchen-room");
+  io.sockets
+    .in("kitchen-room")
+    .emit("cooking", "Fried Rice Cooking= " + room.size);
+
+  socket.join("bed-room");
+  io.sockets.in("bed-room").emit("sleep", "I am sleeping");
+  // find number of members in room
+
+  // console.log(room);
 });
 
 app.get("/", function (req, res) {
